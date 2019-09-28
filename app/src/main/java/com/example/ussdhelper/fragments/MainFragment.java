addb6650c3f079895bdafa230e55e4150d75231e
KamasuPaul <kamasupaul@gmail.com>
2019-09-28 14:17:45 +0300

    added buying airtime

--- app/src/main/java/com/example/ussdhelper/fragments/MainFragment.java
+++ app/src/main/java/com/example/ussdhelper/fragments/MainFragment.java
@@ -1,7 +1,9 @@
 package com.example.ussdhelper.fragments;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -13,6 +15,9 @@ import androidx.lifecycle.Observer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
@@ -68,6 +73,13 @@ public class MainFragment extends Fragment {
                         checkBalance();
                     }
                 });
+        final LinearLayout buyAirtime = root.findViewById(R.id.buy_airtime);
+        buyAirtime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialogPolygon();
+            }
+        });
 //        pageViewModel.getText().observe(this, new Observer<String>() {
 //            @Override
 //            public void onChanged(@Nullable String s) {
@@ -129,5 +141,36 @@ public class MainFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+    private void showDialogPolygon() {
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // before
+        dialog.setContentView(R.layout.dialog_enter_amount);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        dialog.setCancelable(true);
+        final EditText editTextAmount = dialog.findViewById(R.id.edit_text_amount);
+
+
+        ((Button) dialog.findViewById(R.id.bt_okay)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent hoverIntent = new HoverParameters.Builder(getActivity())
+                        .request("46e80959")
+                        .extra("Amount", editTextAmount.getText().toString())
+                        .buildIntent();
+                startActivityForResult(hoverIntent,0);
+            }
+
+        });
+
+        ((Button) dialog.findViewById(R.id.bt_cancel)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+
+        dialog.show();
     }
 }
