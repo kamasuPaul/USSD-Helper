c045250d5e9cbeeacbaeb9f60e4c93fb081f19c0
KamasuPaul <kamasupaul@gmail.com>
2020-05-05 15:25:52 +0300

    -- improved the settings,ui and functionality

--- app/src/main/java/com/quickCodes/quickCodes/SettingsActivity.java
+++ app/src/main/java/com/quickCodes/quickCodes/SettingsActivity.java
@@ -1,12 +1,35 @@
 package com.quickCodes.quickCodes;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-import androidx.appcompat.app.ActionBar;
+import com.balysv.materialripple.MaterialRippleLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    public static void rateAction(Activity activity) {
+        Uri uri = Uri.parse("market://details?id=" + activity.getPackageName());
+        Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+        try {
+            activity.startActivity(goToMarket);
+        } catch (ActivityNotFoundException e) {
+            activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + activity.getPackageName())));
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +39,69 @@ public class SettingsActivity extends AppCompatActivity {
             .beginTransaction()
             .replace(R.id.settings, new SettingsFragment())
             .commit();
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
+
+        setupToolBar();
+    }
+
+    public void showFaq(View view) {
+        startActivity(new Intent(getApplicationContext(), HelpActivity.class));
+
+    }
+
+    //    public void showDialogAbout(View view) {
+//        View aboutView = getLayoutInflater().inflate(R.layout.dialog_about, null);
+//        Dialog d = new Dialog(getApplicationContext());
+//        d.requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        d.setContentView(aboutView);
+//        d.setCancelable(true);
+//        d.show();
+//    }
+    public void showDialogAbout(View view) {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // before
+        dialog.setContentView(R.layout.dialog_about);
+        dialog.setCancelable(true);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(dialog.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+
+        ((TextView) dialog.findViewById(R.id.tv_version)).setText("Version " + BuildConfig.VERSION_NAME);
+
+
+        ((ImageButton) dialog.findViewById(R.id.bt_close)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        ((MaterialRippleLayout) dialog.findViewById(R.id.bt_rate)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rateAction(SettingsActivity.this);
         }
+        });
+
+        dialog.show();
+        dialog.getWindow().setAttributes(lp);
+    }
+
+    public void showPrivacyPolicy(View view) {
+        //TODO add privacy policy url
+        String url = "https://quick-codes.flycricket.io/privacy.html";
+        Intent t = new Intent(Intent.ACTION_VIEW);
+        t.setData(Uri.parse(url));
+        startActivity(t);
+    }
+
+    private void setupToolBar() {
+        Toolbar toolbar = findViewById(R.id.toolbar_settings);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Settings");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
@@ -28,4 +110,5 @@ public class SettingsActivity extends AppCompatActivity {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
         }
     }
+
 }
