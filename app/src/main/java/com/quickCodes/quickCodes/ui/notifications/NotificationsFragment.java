5354e3be2ae2399bf2780117579e65a69748c84b
Kamasu Paul <kamasupaul@gmail.com>
2021-06-29 08:42:19 +0300

    add me fragment

--- app/src/main/java/com/quickCodes/quickCodes/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/quickCodes/quickCodes/ui/notifications/NotificationsFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -23,11 +22,10 @@ public class NotificationsFragment extends Fragment {
         notificationsViewModel =
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+
             }
         });
         return root;
